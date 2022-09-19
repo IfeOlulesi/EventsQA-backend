dd5bf5bfe4643202f7ec468c00cdc9bf07acafdd
IfeOlulesi <ifeolulesi@gmail.com>
2022-09-19 17:38:36 +0100

    Completed Event and Question models

--- mainQA/models.py
+++ mainQA/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Event(models.Model):
@@ -7,7 +8,7 @@ class Event(models.Model):
   eventCode = models.CharField(max_length=20)
   desc = models.CharField(max_length=300)
   dateCreated = models.DateTimeField(default=timezone.now)
-  # createdBy = models.ForeignKey('User', on_delete=models.CASCADE )
+  createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
   location = models.CharField(max_length=100)
   isAcceptingQuestions = models.BooleanField()
 
@@ -15,12 +16,14 @@ class Event(models.Model):
   def __str__(self):
     return self.name
 
+
 class Question(models.Model):
   questionText = models.TextField()
-  eventCode = models.ForeignKey(Event, on_delete=models.CASCADE)
+  eventCode = models.ForeignKey(Event, on_delete=models.CASCADE) #this guy still points to the model, and not the event code
   dateCreated = models.DateTimeField(default=timezone.now)
   createdBy = models.CharField(default="Anonymous",max_length=100)
 
 
   def __str__(self):
-    return self.name  #work on this and return maybe the first 30 characters followed with trailing dots...
+    displayShort = self.eventCode.eventCode + ": " + self.questionText[0:20] + '...'
+    return displayShort  #work on this and return maybe the first 30 characters followed with trailing dots...
---
+++ makemigrations.bat
@@ -0,0 +1,10 @@
+@REM echo "1. Make a migration   2. Migrate"
+
+@REM @echo off
+@REM set /p USER_RESPONSE = ": "
+
+@REM @REM if %USER_RESPONSE%==1 (ping google.com)
+@REM if %USER_RESPONSE%=="1" ("C:\Users\ODULESI\Desktop\Ife\Documents\Career\World of Code\Projects\EventsQA\.venv\Scripts\python.exe" "C:\Users\ODULESI\Desktop\Ife\Documents\Career\World of Code\Projects\EventsQA\manage.py makemigrations")
+@REM if %USER_RESPONSE%==2 ("C:\Users\ODULESI\Desktop\Ife\Documents\Career\World of Code\Projects\EventsQA\.venv\Scripts\python.exe" "C:\Users\ODULESI\Desktop\Ife\Documents\Career\World of Code\Projects\EventsQA\manage.py migrate")
+
+py manage.py makemigrations
