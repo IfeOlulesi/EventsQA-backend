cbb70e622913757cabe669f9777e4e5e0f481034
IfeOlulesi <ifeolulesi@gmail.com>
2022-09-19 16:25:50 +0100

    Created event and question models

--- mainQA/models.py
+++ mainQA/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Event(models.Model):
+  name = models.CharField(max_length=50)
+  eventCode = models.CharField(max_length=20)
+  desc = models.CharField(max_length=300)
+  dateCreated = models.DateTimeField(default=timezone.now)
+  # createdBy = models.ForeignKey('User', on_delete=models.CASCADE )
+  location = models.CharField(max_length=100)
+  isAcceptingQuestions = models.BooleanField()
+
+
+  def __str__(self):
+    return self.name
+
+class Question(models.Model):
+  questionText = models.TextField()
+  eventCode = models.ForeignKey(Event, on_delete=models.CASCADE)
+  dateCreated = models.DateTimeField(default=timezone.now)
+  createdBy = models.CharField(default="Anonymous",max_length=100)
+
+
+  def __str__(self):
+    return self.name  #work on this and return maybe the first 30 characters followed with trailing dots...
